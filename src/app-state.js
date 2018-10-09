3c184e849bf8fc5836a20b5ecfd29f728b0d6942
cjaredm <cjared.mortenson@gmail.com>
2018-10-08 22:43:51 -0600

    Refactor app-state

--- src/app-state.js
+++ src/app-state.js
@@ -1,37 +1,37 @@
 // @flow
 /* eslint-disable react/no-unused-state */
-import React from 'react';
-import type {ComponentType} from 'react';
-import {Dimensions} from 'react-native';
+import React from "react";
+import type { ComponentType } from "react";
+import { Dimensions } from "react-native";
 
 export type AppState = {
   user?: null | Object,
   dims: {
     width: string,
-    height: string,
+    height: string
   }
 };
 
 const {
   Provider,
-  Consumer: AppStateConsumer,
+  Consumer: AppStateConsumer
   // $FlowFixMe
 } = React.createContext();
 
 let instance: AppStateProvider;
 
 class AppStateProvider extends React.Component<
-  {children: React.isValidElement},
+  { children: React.isValidElement },
   AppState
-  > {
+> {
   constructor(...args: Array<any>) {
     super(...args);
-    const dims = Dimensions.get('window');
+    const dims = Dimensions.get("window");
     console.log(dims);
     /** Exposed via context */
     this.state = {
       user: null,
-      dims: dims || {},
+      dims: dims || {}
     };
   }
 
@@ -47,12 +47,26 @@ class AppStateProvider extends React.Component<
 /**
  * Enhance with props {appState: AppState}
  */
-const withAppState = (Inner: ComponentType<Object>) => {
-  return (props: Object) => (
-    <AppStateConsumer>
-      {(appState: AppState) => <Inner appState={appState} {...props} />}
-    </AppStateConsumer>
-  );
+const withAppState = ({ forwardRef = false }: { forwardRef?: boolean }) => (
+  Inner: ComponentType<Object>
+) => {
+  // To avoid react-navigation squawking because it thinks that a forwardRef wrapped
+  // component is not a component... sort of hacky, but at least enables using this HoC with or without forwardRef
+  return forwardRef
+    ? // $FlowFixMe
+      React.forwardRef((props, ref) => (
+        <AppStateConsumer>
+          {(appState: AppState) => (
+            <Inner appState={appState} ref={ref} {...props} />
+          )}
+        </AppStateConsumer>
+      ))
+    : /* eslint-disable */
+      (props: Object) => (
+        <AppStateConsumer>
+          {(appState: AppState) => <Inner appState={appState} {...props} />}
+        </AppStateConsumer>
+      );
 };
 
 /**
@@ -62,4 +76,4 @@ function getAppState(): AppState {
   return instance.state;
 }
 
-export {AppStateProvider, AppStateConsumer, withAppState, getAppState};
+export { AppStateProvider, AppStateConsumer, withAppState, getAppState };
